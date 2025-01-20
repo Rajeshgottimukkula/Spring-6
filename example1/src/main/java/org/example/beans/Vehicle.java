202fe50c8d979c7006d9fe69b4ba1266cba6bdf5
Rajeshgottimukkula <46450473+Rajeshgottimukkula@users.noreply.github.com>
2025-01-20 09:40:44 +0530

    Update Vehicle.java

--- example1/src/main/java/org/example/beans/Vehicle.java
+++ example1/src/main/java/org/example/beans/Vehicle.java
@@ -6,11 +6,13 @@ public class Vehicle {
 
     private String name;
 
+    public Vehicle(String name){
+this.name=name;
+    }
+
     public String getName() {
         return name;
     }
-
-
 
     public void setName(String name) {
         this.name = name;
